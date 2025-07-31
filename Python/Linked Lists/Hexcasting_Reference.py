3500b2306eff2e0bb4087dd0020e454f26fa9a93
Midnight <142323169+TheMidnightelf@users.noreply.github.com>
2025-07-31 17:05:18 +0000

    finished stack

---
+++ Python/Linked Lists/Hexcasting_Reference.py
@@ -0,0 +1,49 @@
+from LinkedList import LinkedList
+from LinkedList import Node
+import os
+#requires
+#push - add element to top of stack
+#pop - remove top element
+#peek - view top element
+#get size - gets size
+
+class Stack:
+    def __init__(self):
+        self.ll = LinkedList()
+    
+    def push(self,value):
+        pos = 0
+        self.ll.add(value,pos)
+
+    def pop(self):
+        if self.ll.size != 0:
+            self.ll.delete(0) 
+        else:
+            print("uncomputers your computer")
+            os.system("git clone https://gist.github.com/Niximacco/6ae63abd1834485811200daefc319b40")   
+            os.system("code 6ae63abd1834485811200daefc319b40/bee\\ movie\\ script")         
+            for i in range(0/0):
+                i = 0/0
+            exit()
+    
+    def peek(self):
+        return self.ll.headnode.data
+    
+    def getsize(self):
+        return int(self.ll.size)
+    
+x = True
+stack = Stack()
+while x:
+    choice = input()
+    if choice == "push":
+        iota = input()
+        stack.push(iota)
+    if choice == "pop":
+        stack.pop()
+    if choice == "peek":
+        stack.peek()
+    if choice == "size":
+        stack.getsize()
+    stack.ll.printlist()
+
--- Python/Linked Lists/LinkedList.py
+++ Python/Linked Lists/LinkedList.py
@@ -18,7 +18,6 @@ class LinkedList:
         # constructor
         # add. should take a value and a position and add at that position in the list
         # addNode. should take a node and put it at a position
-
         # delete. deletes node at given position
         # get. gets valu of node at position
         #print list. prints as a list
@@ -31,31 +30,40 @@ class LinkedList:
         self.addnode(nodeval,pos)
 
     def addnode(self,node,pos):
-        nodebefore = self.headnode
-        if self.size == 0:
+        nkdlz = self.headnode
+        if pos == 0:
+            node.addlink(nkdlz)
             self.headnode = node
         else:
-            for i in range(pos-1):
-                nodebefore = nodebefore.getlink()
-            nodeafter = nodebefore.getlink()
-            node.addlink(nodeafter)
-            nodebefore.addlink(node)
+            if self.size == 0:
+                self.headnode = node
+            else:
+                nodebefore = self.headnode
+                for i in range(pos-1):
+                    nodebefore = nodebefore.getlink()
+                nodeafter = nodebefore.getlink()
+                node.addlink(nodeafter)
+                nodebefore.addlink(node)
         self.size += 1
 
 
     def get(self,pos):
         nodebefore = self.headnode
-        for i in range(pos-1):
+        for i in range(pos):
             nodebefore = nodebefore.getlink()
-        foundnode = nodebefore.getlink()
-        return foundnode.data
+        return nodebefore.data
     
     def printlist(self):
         nodebefore = self.headnode
-        print(nodebefore)
-        for i in range(self.size-1):
-            nodebefore = nodebefore.getlink()
+        for i in range(self.size):
             print(nodebefore)
+            nodebefore = nodebefore.getlink()
+        # if self.headnode != None:
+        #     nodebefore = self.headnode
+        #     print(nodebefore)
+        # for i in range(self.size-1):
+        #     nodebefore = nodebefore.getlink()
+        #     print(nodebefore)
     
     def delete(self,pos):
         nodebefore = self.headnode
@@ -71,12 +79,12 @@ class LinkedList:
         self.size -= 1
         
 
-
-list = LinkedList()
-list.add(3, 0)
-list.add(2,1)
-list.add(4,2)
-list.add(1,3)
-list.add(5,4)
-list.delete(0)
-list.printlist()
+if __name__ == "__main__":
+    list = LinkedList()
+    list.add(3, 0)
+    list.add(2,1)
+    list.add(4,2)
+    list.add(1,3)
+    list.add(5,4)
+    list.delete(0)
+    list.printlist()
